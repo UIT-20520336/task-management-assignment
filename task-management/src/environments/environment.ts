bc201384e180f779fb363237e8ba3f6bd35efc31
Quản Minh Trung <20520336@gm.uit.edu.vn>
2024-11-07 01:20:05 +0700

    change url

--- task-management/src/environments/environment.ts
+++ task-management/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://webapi20241107004245.azurewebsites.net/api'
+  apiBaseUrl: 'https://webapi20241107004245.azurewebsites.net'
 };
